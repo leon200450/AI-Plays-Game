8f177e789c2049c0e3afbf9c9a08248c9ae8e559
USER <leon201650@gmail.com>
2022-09-25 14:11:39 +0800

    essential strcuct and build player1 and pley1.move

---
+++ __init__.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame.locals import *
+
+
+life10_img= pygame.image.load('../assets/LIFE123.PNG')
+
+life9_img= pygame.image.load('../assets/LIFE123-1.PNG')
+
+life8_img = pygame.image.load('../assets/LIFE123-2.PNG')
+life7_img = pygame.image.load('../assets/LIFE123-3.PNG')
+life6_img = pygame.image.load('../assets/LIFE123-4.PNG')
+life5_img = pygame.image.load('../assets/LIFE123-5.PNG')
+life4_img = pygame.image.load('../assets/LIFE123-6.PNG')
+life3_img = pygame.image.load('../assets/LIFE123-7.PNG')
+life2_img = pygame.image.load('../assets/LIFE123-8.PNG')
+life1_img = pygame.image.load('../assets/LIFE123-9.PNG')
+life0_img = pygame.image.load('../assets/LIFE123-10.PNG')
+player_img = pygame.image.load('../assets/playerNomal.png')
+normal_pf_img= pygame.image.load('../assets/playerNomal.png')
+board_pf_img = pygame.image.load('../assets/normal.png')
+nails_pf_img = pygame.image.load('../assets/nails.png')
+wall_img =  pygame.image.load('../assets/wall.png')
+ceil_img= pygame.image.load('../assets/ceiling.png')
+TrampolineUP_img= pygame.image.load('../assets/trampolineUP.png')
+TrampolineDown_img= pygame.image.load('../assets/trampolineDown.png')
+conveyor_left_img= pygame.image.load('../assets/conveyor_left1.png')
+conveyor_right_img= pygame.image.load('../assets/conveyor_right1.png')
+
+walk_right_img_list = [pygame.image.load('../assets/playerR1.png'), pygame.image.load('../assets/playerR2.png')]
+walk_left_img_list  = [pygame.image.load('../assets/playerL1.png'), pygame.image.load('../assets/playerL2.png')]
